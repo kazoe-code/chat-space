c8e1a70d58acab9c073ab86172cef82b5d4c21f0
kazoe <officekazoe@gmail.com>
2020-05-06 16:55:10 +0900

    [WIP]メッセージ送信機能
    # Why
    正しい挙動をしているか確認
    
    # What
    メッセージ送信・画像送信・フラッシュメッセージ完了

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,8 @@
-class Messege < ApplicationRecord
+class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
+
   mount_uploader :image, ImageUploader
 end
